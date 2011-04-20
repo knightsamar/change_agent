97e36db53154492d4b0e881aa12171dae8be7526
apoorva+knightsamar <apoorva+knightsamar@localhost>
2011-04-20 15:30:06 +0530

    template inheritance done by samar. started fixing bug which allows filling already filled forms -- apoorva. wrote the pseudocode for showing summary of feedback submitted.

--- manage_feedback/views.py
+++ manage_feedback/views.py
@@ -3,3 +3,16 @@
 #many of the things here are being managed by the admin panel...so we won't release it in version 0.1
 #one view for Kulkarni Mam and coordinators to see how many and which students in a group hv filled 
 #or not filled the form
+
+##summary of feedback for a form...
+#select a form
+#do we have any submissions for this form ?
+     #if number of submissions is more than 0,
+        print the number of submissions.   
+        
+    #print whether deadline is gone or not for submitting...
+
+    #for each feedbackQuestion in the form
+        #find all AnswerOptions corresponding to it.
+        #for each AnswerOption
+             #count the number of total feedbackSubmissionAnswer...
