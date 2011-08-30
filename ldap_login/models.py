60090017d6784ead169b05629ac25c3d892211f8
apoorva <apoorva@localhost>
2011-08-30 23:59:10 +0530

    added users to SDRC-2011 group using the new script

--- ldap_login/models.py
+++ ldap_login/models.py
@@ -7,7 +7,26 @@ class group(models.Model):
     created_on = models.DateTimeField(auto_now_add=True);
 
     def __str__(self):
-	return self.name;
+       if self.name.isdigit() is True:
+            Three_yr_courses={ 122:'BBA(IT)', 121:'BCA'}
+            Two_yr_courses={ 142:'MSc(CA)', 141:'MBA(IT)'}
+            course=""
+            a=0
+            try:
+                if int(self.name[5:8]) in Three_yr_courses.keys():
+                    a=3
+                    course=Three_yr_courses[int(self.name[5:8])]
+                elif int(self.name[5:8]) in Two_yr_courses.keys():
+                    a=2
+                    course=Two_yr_courses[int(self.name[5:8])]   
+                yr=course+" "+str(self.name[:2]) + '-' + str(a+int(self.name[:2])) 
+            except Exception as e:
+                yr = self.name
+       else:
+            yr=self.name
+       return yr;
+
+
 
 #many user belongs to one group
 class user(models.Model): 
@@ -19,6 +38,6 @@ class user(models.Model):
     allowed_viewing_feedback_about = models.ManyToManyField('manage_feedback.feedbackForm',blank=True,null=True);
 
     def __str__(self):
-	return self.username;
+	 return self.username;
 
 
