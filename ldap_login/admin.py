bb820112e7e5193dce87a71cb222966e4c6513af
apoorva <apoorva@localhost>
2011-12-02 00:26:03 +0530

    did 2 outof 3 jobs given to Samar:P

---
+++ ldap_login/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from ldap_login.models import group,user
+
+class UsersInline(admin.TabularInline):
+    model = user;
+    extra = 1;
+
+class userAdmin(admin.ModelAdmin):
+    search_fields = ['username','fullname'];
+
+class groupAdmin(admin.ModelAdmin):
+	inlines = [UsersInline];
+
+admin.site.register(group);
+admin.site.register(user,userAdmin);
